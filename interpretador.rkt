f60b7d4c0aeb4b72b42afc6d16930a1602dc3014
Juan CV <jacklazerjscv@gmail.com>
2023-10-22 18:43:37 -0500

    Primer commit

---
+++ interpretador.rkt
@@ -0,0 +1,7 @@
+#lang eopl
+
+; Juan Sebastian Cifuentes Vallejo - 202179800
+; Maria Alejandra Carvajal Perez - 202178495
+; Yissy Katherine Posso Perea - 202181910
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
